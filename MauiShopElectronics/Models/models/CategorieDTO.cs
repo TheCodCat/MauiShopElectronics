2d67c1704e6ae14687385cb2ffcf0465085ff48b
TheCodCat <166803529+TheCodCat@users.noreply.github.com>
2025-09-04 19:33:14 +0400

    add edit categorie admin

---
+++ MauiShopElectronics/Models/models/CategorieDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Models.models
+{
+    public class CategorieDTO
+    {
+        public string CategorieTitle { get; set; }
+
+        public CategorieDTO() : this("Нету")
+        {
+
+        }
+
+        public CategorieDTO(string title)
+        {
+            CategorieTitle = title;
+        }
+    }
+}
--- MauiShopElectronics/ViewModels/AdminViewModel.cs
+++ MauiShopElectronics/ViewModels/AdminViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Configuration;
 using Models.models;
 using Newtonsoft.Json;
 using RestSharp;
+using System.Linq;
 
 namespace MauiShopElectronics.ViewModels
 {
@@ -15,7 +17,18 @@ namespace MauiShopElectronics.ViewModels
         private List<Categorie> categories = new List<Categorie>();
 
         [ObservableProperty]
+        private List<string> categoriesString = new List<string>();
+
+        [ObservableProperty]
         private string nameNewCategories = string.Empty;
+
+        [ObservableProperty]
+        private string indexRemoteCategorie;
+
+        partial void OnCategoriesChanging(List<Categorie>? oldValue, List<Categorie> newValue)
+        {
+            CategoriesString = newValue.Select(x => x.Title).ToList();
+        }
 
         public AdminViewModel(IConfiguration configuration)
         {
@@ -24,14 +37,54 @@ namespace MauiShopElectronics.ViewModels
 
         public async void Apperaining()
         {
+            GetCategories();
+        }
+
+        private async void GetCategories()
+        {
             string url = _configuration.GetSection("ConnectionStrings").GetSection("GetCategories").Value;
-            var request = new RestRequest(url,Method.Get);
+            var request = new RestRequest(url, Method.Get);
 
             RestResponse response = await client.ExecuteAsync(request);
-            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 Categories = JsonConvert.DeserializeObject<List<Categorie>>(response.Content);
             }
         }
+
+        [RelayCommand]
+        public async void CreateCategory()
+        {
+            CategorieDTO categorie = new CategorieDTO(NameNewCategories);
+            string json = JsonConvert.SerializeObject(categorie);
+
+            NameNewCategories = string.Empty;
+
+            string url = _configuration.GetSection("ConnectionStrings").GetSection("CreateCategorie").Value;
+
+            RestRequest restRequest = new RestRequest(url, Method.Post);
+            restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.AddParameter("application/json", json, ParameterType.RequestBody);
+
+            var result = await client.ExecuteAsync(restRequest);
+
+            if(result.StatusCode == System.Net.HttpStatusCode.OK)
+                GetCategories();
+
+        }
+
+        [RelayCommand] 
+        public async void RemoveCategory()
+        {
+            if (IndexRemoteCategorie == null) return;
+
+            int index = Categories.FirstOrDefault(x => x.Title == IndexRemoteCategorie).Id;
+            string url = $"{_configuration.GetSection("ConnectionStrings").GetSection("RemoteCategorie").Value}/{index}";
+            var request = new RestRequest(url, Method.Post);
+            RestResponse response = await client.ExecuteAsync(request);
+
+            GetCategories();
+        }
+        
     }
 }
