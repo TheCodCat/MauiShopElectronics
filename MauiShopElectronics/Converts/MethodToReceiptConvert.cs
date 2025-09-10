2b014a3e919018be7b0be0a61db7bc3d09003e2e
TheCodCat <166803529+TheCodCat@users.noreply.github.com>
2025-09-10 13:02:53 +0400

    add convert to records

---
+++ MauiShopElectronics/Converts/MethodToReceiptConvert.cs
@@ -0,0 +1,29 @@
+﻿using Models;
+using System.Globalization;
+
+namespace MauiShopElectronics.Converts
+{
+	public class MethodToReceiptConvert : IValueConverter
+	{
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			if(value is MethodOfReceipt metod)
+			{
+				return metod switch 
+				{
+					MethodOfReceipt.UponReceipt => "Оплата при получении",
+					MethodOfReceipt.PaymentImmediately => "Оплата сразу",
+					_=> "Другой метод"
+				};
+
+			}
+			else
+				return "Другой метод";
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		{
+			return MethodOfReceipt.UponReceipt;
+		}
+	}
+}
